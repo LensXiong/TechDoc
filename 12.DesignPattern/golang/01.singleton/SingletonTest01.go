4f10711e9f04c7996652c21ca174bf0d813d97a4
wangxiong <lensxiong@gmail.com>
2023-03-20 10:26:10 +0800

    add singleton 01

---
+++ 12.DesignPattern/golang/01.singleton/SingletonTest01.go
@@ -0,0 +1,1 @@
+﻿package _1_singleton
--- 12.DesignPattern/java/01.singleton/type1/SingletonTest01.java
+++ 12.DesignPattern/java/01.singleton/type1/SingletonTest01.java
@@ -1,11 +1,15 @@
 package com.wwxiong.singleton.type1;
 
+// 本段代码实现了饿汉式单例模式。
+// 单例模式是一种创建型设计模式，它确保一个类只有一个实例，并提供对该实例的全局访问点。
+
 public class SingletonTest01 {
 
 	public static void main(String[] args) {
-		//测试
+		// 创建 Singleton 类的实例并使用 getInstance() 方法
 		Singleton instance = Singleton.getInstance();
 		Singleton instance2 = Singleton.getInstance();
+		// 比较两个实例引用是否相等
 		System.out.println(instance == instance2); // true
 		System.out.println("instance.hashCode=" + instance.hashCode());
 		System.out.println("instance2.hashCode=" + instance2.hashCode());
@@ -13,19 +17,22 @@ public class SingletonTest01 {
 
 }
 
-//饿汉式(静态变量)
+// Singleton 类是单例模式的实现类，它有以下特点：
+// 构造器私有化，外部无法通过 new 关键字创建实例，只能通过 getInstance() 方法获取单例。
+// 本类内部创建一个私有静态变量 instance，它是 Singleton 类的唯一实例。在类加载时就创建了该实例，因此被称为“饿汉式”。
+// 提供一个公有的静态方法 getInstance()，该方法返回 Singleton 类的唯一实例 instance。
 
 class Singleton {
 	
-	//1. 构造器私有化, 外部能new
+	// 构造器私有化，外部无法通过 new 关键字创建实例
 	private Singleton() {
 		
 	}
 	
-	//2.本类内部创建对象实例
+	// 本类内部创建一个私有静态变量 instance，它是 Singleton 类的唯一实例
 	private final static Singleton instance = new Singleton();
 	
-	//3. 提供一个公有的静态方法，返回实例对象
+	// 提供一个公有的静态方法 getInstance()，返回实例对象
 	public static Singleton getInstance() {
 		return instance;
 	}
